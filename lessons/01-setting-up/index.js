46430c5263b1dc9a44d1a20a13083e7ce125d131
kelvin knigton <keltheceo@gmail.com>
2016-05-26 19:19:37 -0700

    1st commit

---
+++ lessons/01-setting-up/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Cat from './modules/Cat'
+render(<Cat/>, document.getElementById('app'))
---
+++ lessons/01-setting-up/modules/Cat.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default React.createClass({
+  render() {
+    return <div>a big Hello from React Router!!!</div>
+  }
+})
